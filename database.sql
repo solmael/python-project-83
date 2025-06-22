97bc2f0d0751e71ba6be5c330f168c557846c2c4
solmael <solmael13@gmail.com>
2025-06-22 19:44:58 +0300

    database upd

---
+++ database.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS url_checks;
+DROP TABLE IF EXISTS urls;
+
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL CHECK (name <> ''),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL REFERENCES urls(id),
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
