fe73b7a5efee2ec84f461cd4933644e4fb6e2c21
solmael <solmael13@gmail.com>
2025-06-26 13:13:55 +0300

    parser added

---
+++ page_analyzer/parser.py
@@ -0,0 +1,39 @@
+import logging
+from typing import Any, Dict
+
+from bs4 import BeautifulSoup
+
+
+class PageParseError(Exception):
+    pass
+
+
+logger = logging.getLogger(__name__)
+
+
+def parse_page(
+        html_content: str, 
+        url: str, 
+        encoding: str = 'utf-8'
+        ) -> Dict[str, Any]:
+    try:
+        logger.debug(f"Парсинг страницы: {url}")
+        soup = BeautifulSoup(
+            html_content, 
+            'html.parser', 
+            from_encoding=encoding
+            )
+        
+        description_tag = soup.find('meta', attrs={"name": "description"})
+        description = description_tag.get(
+            'content', ''
+            ).strip() if description_tag else None
+
+        return {
+            'h1': soup.find('h1').text.strip() if soup.find('h1') else None,
+            'title': soup.title.string.strip() if soup.title else None,
+            'description': description
+        }
+    except Exception as e:
+        logger.error(f"Ошибка парсинга {url}: {e}")
+        raise PageParseError(f"Ошибка парсинга страницы {url}: {e}")
--- page_analyzer/url_validator.py
+++ page_analyzer/url_validator.py
@@ -1,4 +1,5 @@
 from urllib.parse import urlparse
+
 import validators
 
 
